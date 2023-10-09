5083c5d3b141c7e1744908ece5be2cf70e83172a
liwenjing <liwenjing@rong360.com>
2023-10-09 19:07:29 +0800

    build

---
+++ assets/cooking-ef423b24.js
@@ -0,0 +1,1 @@
+import{d as o,o as t,c as n,a as s}from"./app-16f6e0b8.js";const c={class:"markdown-body"},a=s("h1",null,"Hi, Cooking",-1),r=[a],m={},l="",f=o({__name:"cooking",setup(_,{expose:e}){return e({frontmatter:{},excerpt:void 0}),(d,i)=>(t(),n("div",c,r))}});export{f as default,l as excerpt,m as frontmatter};
