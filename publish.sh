f3ec75ad9d9b5e64650ead1db57686878e691a4a
liwenjing <liwenjing@rong360.com>
2023-10-23 17:03:32 +0800

    update

--- publish.sh
+++ publish.sh
@@ -4,4 +4,6 @@
 
 # git commit -m "update"
 
-git subtree push --prefix dist origin gh-pages
+git subtree split --prefix dist/ -b gh-pages
+git push -f origin gh-pages:gh-pages
+# git branch -D gh-pages
